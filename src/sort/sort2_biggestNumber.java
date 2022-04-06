25f235a5e1b7810492ae0f627b60ce38c5046e43
eeun <eeun95@gmail.com>
2022-04-06 16:46:27 +0900

    코딩테스트

--- src/sort/sort2_biggestNumber.java
+++ src/sort/sort2_biggestNumber.java
@@ -1,11 +1,29 @@
 package sort;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class sort2_biggestNumber {
     public static void main(String[] args) {
+        int [] numbers1 = {6, 10, 2};
+        int [] numbers2 = {3, 30, 34, 5, 9};
 
+        System.out.println(solution(numbers2));
     }
-    public String solution(int[] numbers) {
+    public static String solution(int[] numbers) {
         String answer = "";
+
+        String [] temp = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            temp[i] = Integer.toString(numbers[i]);
+        }
+        Arrays.sort(temp);
+        String s = "";
+        for (int j = temp.length; j > 0; j--) {
+            s += temp[j-1];
+        }
+        //System.out.println(s);
+        answer = s;
         return answer;
     }
 }
