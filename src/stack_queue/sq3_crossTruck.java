0e51b4ac77496d83c2c3bd38914892c1fbffb4b9
eeun <eeun95@gmail.com>
2022-04-11 22:49:34 +0900

    코딩테스트 스택/큐

--- src/stack_queue/sq3_crossTruck.java
+++ src/stack_queue/sq3_crossTruck.java
@@ -2,6 +2,11 @@ package stack_queue;
 
 public class sq3_crossTruck {
     public static void main(String[] args) {
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7, 4, 5, 6};
+
+        System.out.println(solution(bridge_length, weight, truck_weights));
 
     }
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
--- src/stack_queue/sq4_stockPrice.java
+++ src/stack_queue/sq4_stockPrice.java
@@ -2,10 +2,13 @@ package stack_queue;
 
 public class sq4_stockPrice {
     public static void main(String[] args) {
-
+        int[] prices = {1, 2, 3, 2, 3};
+        System.out.println(solution(prices));
     }
     public static int[] solution(int[] prices) {
         int[] answer = {};
+
+        System.out.println("tt");
         return answer;
     }
 }
