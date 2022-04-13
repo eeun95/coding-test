cdf0d15fbdff331119a7fff806565817fdc09481
eeun <eeun95@gmail.com>
2022-04-13 13:22:11 +0900

    코딩테스트 스택/큐

--- src/stack_queue/sq4_stockPrice.java
+++ src/stack_queue/sq4_stockPrice.java
@@ -2,13 +2,29 @@ package stack_queue;
 
 public class sq4_stockPrice {
     public static void main(String[] args) {
-        int[] prices = {1, 2, 3, 2, 3};
+        int[] prices = {1, 2, 3, 2, 3};     // 4 3 1 1 0
         System.out.println(solution(prices));
     }
     public static int[] solution(int[] prices) {
         int[] answer = {};
 
-        System.out.println("tt");
+        int max_sec = prices.length;
+        answer = new int[max_sec];
+
+        for (int i = 0; i < max_sec; i++) {
+            int sec = 0;
+            for (int j = i + 1; j < max_sec; j++) {
+                System.out.println(prices[i] + " " + prices[j]);
+                if(prices[i] <= prices[j]) {
+                    sec++;
+                } else {
+                    sec++;
+                    break;
+                }
+            }
+            answer[i] = sec;
+        }
+
         return answer;
     }
 }
