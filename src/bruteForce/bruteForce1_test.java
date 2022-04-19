b517678c3f4a03cfef96d7338e237dac86bd6c08
eeun <eeun95@gmail.com>
2022-04-19 23:05:27 +0900

    코딩테스트 완전탐색

--- src/bruteForce/bruteForce1_test.java
+++ src/bruteForce/bruteForce1_test.java
@@ -7,8 +7,8 @@ public class bruteForce1_test {
         int answers[] = {1, 2, 3, 4, 5};
         int answers2[] = {1, 3, 2, 4, 2};
         int answers3[] = {3, 3, 2, 1, 5};
-        int answers4[] = {5, 5, 4, 3, 3};
-        System.out.println(solution(answers4));
+        int answers4[] = {5, 5, 4, 3, 3,6,6,6,6,6,6};
+        System.out.println(solution(answers3));
     }
 
     public static int[] solution(int[] answers) {
@@ -20,8 +20,10 @@ public class bruteForce1_test {
 
         for (int i = 0; i < students.length; i++) {
             for (int j = 0; j < answers.length; j++) {
-                if (students[i][j] == answers[j]) {
+                if (students[i][j%5] == answers[j]) {
                     hm.put((i+1), hm.getOrDefault((i+1), 0)+1);
+                } else {
+                    hm.put((i + 1), hm.getOrDefault((i+1), 0));
                 }
             }
         }
@@ -33,12 +35,13 @@ public class bruteForce1_test {
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        if(keySet.size()>0) {
             int max = hm.get(keySet.get(0));            // 젤 많이 맞춘 애가 맞춘 갯수
             for (int n : hm.keySet()) {
                 if (max == hm.get(n)) {
                     System.out.println(max + " student" + n + " 바로 최대값");
                     pq.add(n);
+                } else {
+                    System.out.println(hm.get(n) + " student" + n + " 안최대값");
                 }
             }
 
@@ -48,9 +51,6 @@ public class bruteForce1_test {
                 answer[idx] = pq.poll();
                 idx++;
             }
-        } else {
-            answer = new int[] {1,2,3};
-        }
 
         return answer;
     }
