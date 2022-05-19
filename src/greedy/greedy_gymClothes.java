de17e73bd917f16aedf89d07a525be64655cead2
eeun <eeun95@gmail.com>
2022-05-19 17:38:45 +0900

    그리디

--- src/greedy/greedy_gymClothes.java
+++ src/greedy/greedy_gymClothes.java
@@ -1,61 +1,87 @@
 package greedy;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class greedy_gymClothes {
     public static void main(String[] args) {
         int n = 5;
         int[] lost = {1, 2, 4};
         int[] reserve = {2, 4, 5}; // 정답 4
-        System.out.println("return : "+solution(n, lost, reserve));
 
+        n = 5;
         lost = new int[] {1, 2, 4};
         reserve = new int[] {2, 3, 4, 5}; // 정답 4
-        System.out.println("return : "+solution(n, lost, reserve));
 
-        lost = new int[]{1,3,5};
-        reserve = new int[]{1,3,5};
         System.out.println("return : "+solution(n, lost, reserve));
+        n = 5;
+        lost = new int[] { 2, 4};
+        reserve = new int[] {1,3, 5}; // 정답 5
+
+
     }
 
     public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
 
-        int l_cnt = lost.length;
-        int r_cnt = reserve.length;
-        int s_cnt = n-(l_cnt+r_cnt);
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
 
-        int cnt = 0;
+        HashSet<Integer> set = new HashSet<>();
+        Queue<Integer> rq = new LinkedList<>();
+        Queue<Integer> lq = new LinkedList<>();
+        Queue<Integer> dq = new LinkedList<>();
 
-        for (int i : reserve) {
+        int cnt = reserve.length;
 
-            for (int j : lost) {
-               //System.out.println(i + "번 학생 > " + j+"번 학생");
+        for (int l : lost) {
+            set.add(l);
+            lq.add(l);
+        }
+        for (int r : reserve) {
+            set.add(r);
+            rq.add(r);
 
-                int max = i+1;
-                int min = i-1;
+            if (lq.contains(r)) {
+                lq.remove(r);
+                rq.remove(r);
+                dq.add(r);
+            }
+        }
 
-                // 빌려줄 수 있는 지 확인
-                if (j==max || j==min) {
-                    cnt++;
+        System.out.println(lq.size()+" "+rq.size());
+
+        for (int r : reserve) {
+
+            if (dq.contains(r)) {
+                continue;
+            }
+
+            int idx = 0;
+            while (idx < lq.size()) {
+                if (lq.isEmpty()) {
                     break;
                 }
+                int max = r+1;
+                int min = r-1;
+
+                int l = lq.poll();
+
+                if (l == max || l == min) {
+                    System.out.println(r+"번 학생이 "+l+"번 학생에게 옷 빌려줌");
+                    lq.remove(l);
+                    cnt++;
+                    break;
+                } else {
+                    lq.add(l);
+                }
+
+                idx ++;
             }
         }
-        if (cnt >= l_cnt) {
-            // 다 빌려줄 수 있음
-            answer = n;
-        } else {
-            answer = s_cnt + r_cnt + cnt;
-        }
-        System.out.println(">>>"+cnt);
 
-        for (int i : lost) {
-            if(Arrays.asList(reserve).contains(i)) {
-                System.out.println(i);
-            }
-        }
-
+        int s_cnt = n- set.size();      // 체육수업 무조건 듣는 학생 수
+        System.out.println(s_cnt+" + "+cnt);
+        answer = s_cnt + cnt;
 
         return answer;
     }
