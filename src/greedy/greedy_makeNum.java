eec1fcc480f40e54f5aa1c055e618d281a156fd2
eeun <eeun95@gmail.com>
2022-05-24 20:22:03 +0900

    그리디

---
+++ src/greedy/greedy_makeNum.java
@@ -0,0 +1,47 @@
+package greedy;
+
+
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+public class greedy_makeNum {
+    public static void main(String[] args) {
+        solution("4177252841", 4);
+    }
+
+    public static String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder("");
+        int len = number.length() - k;
+        int start = 0;
+
+        while(start < number.length() && answer.length() != len) {
+            int leftNum = k + answer.length() + 1;
+            int max = 0;
+            for (int j = start; j < leftNum; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            answer.append(Integer.toString(max));
+        }
+        return answer.toString();
+    }
+
+    public String solution2(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }
+}
