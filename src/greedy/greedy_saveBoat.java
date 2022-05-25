0c29d565dfdec8ae216902c20dbc0a43d57416a5
eeun <eeun95@gmail.com>
2022-05-25 14:38:34 +0900

    그리디

--- src/greedy/greedy_saveBoat.java
+++ src/greedy/greedy_saveBoat.java
@@ -16,24 +16,34 @@ public class greedy_saveBoat {
 
         int save = 0;
         int one_boat = 0;
+        boolean boat_flag = true;
 
         for (int i = 0; i < people.length; i++) {
             save += people[i];
             System.out.println(save);
             if(limit >= save) {
                 System.out.println("한 보트에 타");
-                if(one_boat > 2) {
+
+                if(boat_flag) {
+                    // 첫번째로 꺼낸 보트
                     answer++;
+                    boat_flag = false;
+                }
+                if(one_boat > 2) {
+                    System.out.println("한 보트에 두명 다 탐 새보트 꺼낼차례");
                     one_boat = 0;
+                    save = people[i];
+                    answer++;
                 }
                 one_boat++;
             } else {
                 save = people[i];
                 System.out.println("새 보트 꺼내");
                 answer++;
+                boat_flag = true;
             }
         }
-        System.out.println(answer+one_boat);
+        System.out.println(answer);
         return answer;
     }
 }
