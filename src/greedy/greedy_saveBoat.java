4d3e3b41f7866b30b6724f7ff961e0eb70756cfc
eeun <eeun95@gmail.com>
2022-05-25 15:10:39 +0900

    그리디

--- src/greedy/greedy_saveBoat.java
+++ src/greedy/greedy_saveBoat.java
@@ -1,5 +1,6 @@
 package greedy;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class greedy_saveBoat {
@@ -7,10 +8,10 @@ public class greedy_saveBoat {
         int[] people = {70, 50, 80, 50};
         int limit = 100;
 
+        solution(people, limit);
         people = new int[]{40, 50, 150, 160};
         limit = 200;
 
-        solution(people, limit);
         people = new int[]{100, 500, 500, 900, 950};
         limit = 1000;
 
@@ -18,6 +19,24 @@ public class greedy_saveBoat {
     public static int solution(int[] people, int limit) {
         int answer = 0;
 
+        Arrays.sort(people);
+
+        int new_boat = 0;
+        for (int i = 0; i < people.length; i++) {
+
+            for (int j = people.length - 1; j > i; j--) {
+
+                int save = people[i] + people[j];
+                if(save <= limit ) {
+                    System.out.println(people[i] + " " + people[j]+" 보트 하나 탑승");
+                    new_boat++;
+                    break;
+                } else {
+                    System.out.println(people[i] + " " + people[j]+" 새보트");
+                    new_boat++;
+                }
+            }
+        }
         return answer;
     }
 }
