7090089ec312be04356930a65c77a2d083c81107
eeun <eeun95@gmail.com>
2022-10-27 22:05:06 +0900

    백준

---
+++ src/baekjoon/baekjoon16953.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon16953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+    }
+}
