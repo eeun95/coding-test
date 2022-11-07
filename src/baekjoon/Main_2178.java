88b2c47ca781ae1c3b868b4fcc856c3056138509
eeun <eeun95@gmail.com>
2022-11-08 00:52:18 +0900

    백준

--- src/baekjoon/Main_2178.java
+++ src/baekjoon/Main_2178.java
@@ -9,36 +9,41 @@ public class Main_2178 {
     static int N;
     static int M;
 
-    static int[][] dfs;
-    static int[][] visit;
+    static int[][] find;
+    static boolean[][] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.valueOf(st.nextToken());    // 줄의 갯수
         M = Integer.valueOf(st.nextToken());    // 칸의 갯수
 
-        dfs = new int[N + 1][M + 1];
+        find = new int[N + 1][M + 1];
+        visit = new boolean[N + 1][M + 1];
+
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < M; j++) {
                 int ss = Integer.valueOf(s.substring(j, j + 1));
-                dfs[i][j] = ss;
+                find[i][j] = ss;
             }
         }
-        dfs(0,0);
+        move(0,0);
     }
-    public static void dfs(int i, int j) {
+    public static void move(int i, int j) {
         if(i<N && j<M) {
-            if (dfs[i][j] == 1) {
+            if (find[i][j] == 1) {
+                visit[i][j]= true;
+                System.out.println(i+" "+j+" 통로");
                 for (int k = j+1; k < M; k++) {
-                    System.out.println(i+" "+k+" 지나가");
-                    dfs(i, k);
+                    move(i, k);
                 }
             } else {
                 i++;
-                System.out.println(i+" "+(j-1)+" 내려가");
-                dfs(i, j-1);
+                System.out.println(">>"+i+" "+(j-1));
+                move(i, j-1);
             }
+        } else {
+            System.out.println("넘음");
         }
     }
 }
