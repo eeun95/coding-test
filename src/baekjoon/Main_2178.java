6a374753be5c4dadf3d1a235d944ce21482266d8
eeun <eeun95@gmail.com>
2022-11-07 23:51:31 +0900

    백준

--- src/baekjoon/Main_2178.java
+++ src/baekjoon/Main_2178.java
@@ -9,19 +9,25 @@ public class Main_2178 {
     static int N;
     static int M;
 
-    static boolean[][] dfs;
-    static boolean[] visit;
+    static int[][] dfs;
+    static int[][] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.valueOf(st.nextToken());    // 줄의 갯수
         M = Integer.valueOf(st.nextToken());    // 칸의 갯수
 
-        dfs = new boolean[N + 1][M + 1];
+        dfs = new int[N + 1][M + 1];
         for (int i = 0; i < N; i++) {
+            String s = br.readLine();
             for (int j = 0; j < M; j++) {
-
+                int ss = Integer.valueOf(s.substring(j, j + 1));
+                dfs[i][j] = ss;
             }
         }
+        dfs(N, M);
+    }
+    public static void dfs(int i, int j) {
+
     }
 }
