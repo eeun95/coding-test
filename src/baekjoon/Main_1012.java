0a85c83a150940090e3657fc6699979395d1e991
eeun <eeun95@gmail.com>
2022-11-09 20:43:36 +0900

    백준

---
+++ src/baekjoon/Main_1012.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.valueOf(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int M = Integer.valueOf(st.nextToken());
+            int N = Integer.valueOf(st.nextToken());
+            int K = Integer.valueOf(st.nextToken());
+
+            for (int j = 0; j < M; j++) {
+
+            }
+
+        }
+    }
+}
