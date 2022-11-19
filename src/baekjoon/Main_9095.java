e334e0ff3001ba1cd280d367c58d72de7d720acd
eeun <eeun95@gmail.com>
2022-11-20 00:00:06 +0900

    백준

---
+++ src/baekjoon/Main_9095.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_9095 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = sc.nextInt();
+        }
+    }
+}
