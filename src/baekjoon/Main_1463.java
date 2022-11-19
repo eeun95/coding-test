b25d5011daa47dc22c876ceb4e4a97456858fecd
eeun <eeun95@gmail.com>
2022-11-19 20:13:35 +0900

    백준

--- src/baekjoon/Main_1463.java
+++ src/baekjoon/Main_1463.java
@@ -15,6 +15,8 @@ public class Main_1463 {
             dp[i] = dp[i-1] + 1;
             if(i%2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
             if(i%3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+            System.out.println(i + ">" + dp[i]+" "+dp[1]);
+
         }
         System.out.println(dp[N]);
     }
