0d7a256a925cd7c3c794088a1b99a31c15bf1d0f
eeun <eeun95@gmail.com>
2022-11-26 20:46:17 +0900

    백준

--- src/baekjoon/Main_15989.java
+++ src/baekjoon/Main_15989.java
@@ -3,45 +3,40 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main_15989 {
-    static int[] dp;
     public static void main(String[] args) {
         // 각 테스트 케이스마다 n을 1,2,3의 합으로 나타내는 방법의 수를 출력한다
         // 합을 이루고 있는 수의 순서만 다른 것은 같은 것으로 친다
         Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
         int T = sc.nextInt();
         int[] result = new int[T];
+        int[][] dp = new int[10001][4];
+
+        dp[1][1] = 1;
+        dp[1][2] = 0;
+        dp[1][3] = 0;
+
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[2][3] = 0;
+
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= 10000; i++) {
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] +dp[i-3][2]+dp[i-3][3];
+        }
+
         for (int i = 0; i < T; i++) {
             int n = sc.nextInt();
-            dp = new int[n+1];
-            result[i] = n;
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3]+"\n");
         }
-        for(int r : result) {
-            for (int j = 1; j <= r; j++) {
-                //System.out.println(j+" "+dp(j));
-                dp(j);
-            }
-            System.out.println(dp[r]);
-        }
-    }
-    public static int dp(int n) {
-        if(n<4) {
-            return dp[n] = n;
-        } else {
-            if(n<6) {
-                return dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3] - (dp[n - 1] - 1) - (n/5);
-            } else if(n%5==0) {
-                return dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3] - (dp[n - 1] - 1) - 5;
-            }else {
-                //System.out.println(n+" "+(n%5)+" "+(dp[n-1]-1));
-                return dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3] - (dp[n - 1] - 1) - ((n%5));
-            }
-
-        }
+        System.out.println(sb);
+        Runtime.getRuntime().gc();
+        long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.print(usedMemory + " bytes");
     }
 }
-
-/*
-* 4 (1+1+1+1) (1+2+1) (1+3) (2+2) = 4
-* 7 (1+1+1+1+1+1+1) (1+1+1+1+1+2) (1+1+1+2+2) (1+2+2+2) (1+1+1+1+3) (1+3+3) (1+1+2+3) (2+2+3) = 8
-* 10
-* */
