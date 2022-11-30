f0ae735c44d90513023a6b30916167eb6e73912e
eeun <eeun95@gmail.com>
2022-11-30 23:23:58 +0900

    백준

--- src/baekjoon/Main_2667.java
+++ src/baekjoon/Main_2667.java
@@ -1,53 +1,77 @@
+package baekjoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main_2667 {
     static boolean[][] dfs;
+    static boolean[][] bfs;
     static boolean[][] visit;
+    static int[] dX = {0, 0, -1, 1};
+    static int[] dY = {-1, 1, 0, 0};
     static int cnt,index=0;
     static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.valueOf(br.readLine());
         dfs= new boolean[N+1][N+1];
+        bfs = new boolean[N + 1][N + 1];
         visit = new boolean[N+1][N+1];
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < s.length(); j++) {
                 boolean b = (s.substring(j, j+1).equals("0")) ? false : true;
                 dfs[i][j] = b;
+                bfs[i][j] = b;
             }
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                int c = dfs(i,j);
-                //System.out.println(c+"======");
+                //int c = dfs(i,j);
+
+                int c = bfs(i, j);
                 if(c>0) {
-                    map.put(index, c);
-                    index++;
+                    list.add(c);
                 }
             }
         }
-        System.out.println(map.size());
 
-        List<Integer> keySet = new ArrayList<>(map.keySet());
+        Collections.sort(list);
 
-        keySet.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return map.get(o1).compareTo(map.get(o2));
-            }
-        });
-
-
-        for (int k : keySet) {
-            System.out.println(map.get(k));
+        System.out.println(list.size());
+        for (int k : list) {
+            System.out.println(k);
         }
+    }
+    public static int bfs(int x, int y) {
+        int cnt = 0;
+        Queue<int[]> q = new LinkedList<>();
+        if(!visit[x][y] && bfs[x][y]) {
+            cnt++;
+            visit[x][y] = true;
+            q.add(new int[]{x, y});
+            while (!q.isEmpty()) {
+                int[] xy = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int dx = xy[0] + dX[i];
+                    int dy = xy[1] + dY[i];
+
+                    if (dx >= 0 && dx < N && dy >= 0 && dy < N) {
+                        if (bfs[dx][dy] && !visit[dx][dy]) {
+                            //System.out.println(x+" "+y+" "+dx+" "+dy);
+                            visit[dx][dy] = true;
+                            q.add(new int[]{dx, dy});
+                            cnt++;
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
     }
     public static int dfs(int dx, int dy) {
         if(!visit[dx][dy]) {
