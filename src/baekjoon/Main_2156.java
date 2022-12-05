6965807b07e2d2161985a06f2d1b3c806f2b9a9b
eeun <eeun95@gmail.com>
2022-12-05 23:46:40 +0900

    백준

--- src/baekjoon/Main_2156.java
+++ src/baekjoon/Main_2156.java
@@ -3,25 +3,31 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main_2156 {
-    static int[] amount, dp;
-    static int drink, max = 0;
     public static void main(String[] args) {
         // 잔에 있는 건 다 마셔야하고 마신 후 제자리에 둬야함
         // 연속 3잔을 마실 수 없음
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();           // 포도주 잔의 개수
-        amount = new int[n + 1];        // 포도주 양을 저장한 배열
-        //drink = new int[n + 1];         // 마신 포도주 순서 (0 1 2 ..)
-        max = n - (n/3);                // 최대 마실 수 있는 잔
-        System.out.println("최대 마실 수 있는 잔 "+max);
-        for (int i = 0; i < n; i++) {
-            amount[i] = sc.nextInt();   // 마실 수 있는 포도주의 양
+        int n = sc.nextInt();                // 포도주 잔의 개수
+        int[] amount = new int[n + 1];       // 포도주 양을 저장한 배열
+        int[] dp = new int[n+1];
+
+        // 잔이 3개 미만일 경우 모든 잔을 먹어야 최댓값
+        for (int i = 1; i <= n; i++) {
+            amount[i] = sc.nextInt();
+            if(i<3) {
+                dp[i] += amount[i] + dp[i-1];
+            }
         }
-    }
-    public static void drink() {
-        drink++;
-    }
-    public static void dp() {
+
+        // O O X 전전 와인을 먹고 현재 와인을 먹는다
+        // O X O 전 와인을 먹고 현재 와인을 먹는다
+        // X O O 전과 전전 와인을 먹고 현재 와인을 먹지 않는다
+        // 세 개의 경우의 수가 있음 (3잔을 연달아 마시면 안되므로)
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2], dp[i-3] + amount[i-1]) + amount[i]);
+        }
+
+        System.out.println(dp[n]);
 
     }
 }
