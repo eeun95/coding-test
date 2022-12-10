6d2d65a3e32d0dffe94f8f2f9aefab8cb552c527
eeun <eeun95@gmail.com>
2022-12-10 23:01:37 +0900

    백준

--- src/baekjoon/Main_1309.java
+++ src/baekjoon/Main_1309.java
@@ -9,16 +9,18 @@ public class Main_1309 {
         int N = sc.nextInt();       // 사자 우리의 크기
         int[][] dp = new int[N + 1][N + 1];
 
-        dp[N][0] = 1;               // 한마리도 배치하지 않을 경우 = 1
-        dp[N][1] = (2*N);           // 한마리씩 배치할 경우 가로X세로
-        dp[N][N] = 2;               // 우리가 꽉 찬 지그재그 배치 = 2
-
-        // 추가되는 줄 아래는 직전 데이터 . .
-        for (int i = 3; i <= N; i++) {
-            dp[N][i] = 0 + dp[N-1][i];
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = 1;           // 한마리도 배치하지 않을 경우 = 1
+            dp[i][1] = (2*N);       // 한마리씩 배치할 경우 가로X세로
+            dp[i][N] = 2;           // 우리가 꽉 찬 지그재그 배치 = 2
+            for (int j = 2; j < i; j++) {
+                //System.out.println("dp[" + (i - 1) + "][" + (j) + "]");
+                dp[i][j] = (2 * (2 * (N - 1)-1)) + dp[i - 1][j];
+                //System.out.println((2 * (2 * (N - 1)-1)) + " + " + dp[i - 1][j]);
+            }
         }
-
         int sum = Arrays.stream(dp[N]).sum();
+        System.out.println(sum%9901);
 
     }
 }
