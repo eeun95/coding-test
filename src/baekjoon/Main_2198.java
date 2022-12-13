31c090af7e966d0edf40c28030f5ef1d5f68cf3d
eeun <eeun95@gmail.com>
2022-12-13 23:12:13 +0900

    백준

---
+++ src/baekjoon/Main_2198.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_2198 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        Long[][] dp = new Long[N+1][2];
+        dp[1][0] = 0L;       // 한자리 수 중 0으로 끝나는 이친수 갯수
+        dp[1][1] = 1L;       // 한자리 수 중 1로 끝나는 이친수 갯수
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i-1][0]+dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
+            System.out.println(dp[i-1][0]+" "+dp[i-1][1]);
+        }
+
+        System.out.println(dp[N][0]+dp[N][1]);
+
+    }
+}
