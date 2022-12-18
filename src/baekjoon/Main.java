7229e2d98c20143533bcb747de0d04a1da5dc729
eeun <eeun95@gmail.com>
2022-12-18 20:30:18 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,17 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        int H = sc.nextInt();
+        int M = sc.nextInt();
 
-        if(x>0 && y>0) {
-            System.out.println(1);
-        } else if(x<0 && y>0) {
-            System.out.println(2);
-        } else if(x<0 && y<0) {
-            System.out.println(3);
-        } else if(x>0 && y<0) {
-            System.out.println(4);
+        int h, m =0;
+        if(M>=45) {
+            h = H;
+            m = M-45;
+        } else {
+            h = H-1;
+            m = (60+M)-45;
         }
+        if(h<0) {
+            h = (24+h);
+        }
+        System.out.println(h+" "+m);
     }
 }
