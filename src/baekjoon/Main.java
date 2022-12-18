d1ae1151ed2f218dd7a6d9bf694465f142e50f5a
eeun <eeun95@gmail.com>
2022-12-18 20:13:03 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int A = sc.nextInt();
-        String b = sc.next();
-
-        System.out.println(A*(Integer.parseInt(b.substring(2))));
-        System.out.println(A*(Integer.parseInt(b.substring(1,2))));
-        System.out.println(A*(Integer.parseInt(b.substring(0,1))));
-        System.out.println(A*Integer.parseInt(b));
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
     }
 }
