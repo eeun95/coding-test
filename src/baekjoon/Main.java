b14bd4dd2f7a8a2fd0fa8795998a23d99745d004
eeun <eeun95@gmail.com>
2022-12-18 20:15:34 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
     }
 }
