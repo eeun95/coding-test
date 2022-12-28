2c670b8e41780880a674591a44cdeabe349b7a39
eeun <eeun95@gmail.com>
2022-12-28 14:16:48 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,26 +6,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int N = Integer.parseInt(st.nextToken());
-
-            double[] score = new double[N];
-            for (int j = 0; j < N; j++) {
-                score[j] = Double.parseDouble(st.nextToken());
-            }
-            double avg = Arrays.stream(score).sum()/N;
-            double high=0;
-            for (int k = 0; k < N; k++) {
-                if (score[k] > avg) {
-                    high++;
-                }
-            }
-            System.out.println(String.format("%.3f",high/N*100)+"%");
-
-        }
     }
+    public int Test(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+
 }
