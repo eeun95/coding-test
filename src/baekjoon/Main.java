5ffaffdcf77a23b69491601181f56ed515dfb50a
eeun <eeun95@gmail.com>
2022-12-31 16:15:24 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -8,24 +8,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-
-        String[] alphabet = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        int T = Integer.parseInt(br.readLine());
 
         Map<String, Integer> map = new HashMap<>();
-        for (String a : alphabet) {
-            map.put(a, -1);
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int R = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+            map.put(s, R);
         }
-        for (int i = 0; i < s.length(); i++) {
-            String ss = s.substring(i, i + 1);
-            //System.out.println(i+" "+ss);
-            if(map.get(ss) == -1) {
-                map.put(ss, i);
+
+        for (String s : map.keySet()) {
+            for (int j = 0; j < s.length(); j++) {
+                String ss = s.substring(j, j + 1);
+                System.out.print(ss.repeat(map.get(s)));
             }
-        }
-        for (String key : map.keySet()) {
-            System.out.print(map.get(key)+" ");
+            System.out.println();
+
         }
     }
 }
