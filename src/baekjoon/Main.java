5efd85b82750bfa193fe4159c5f79a675ac1302d
eeun <eeun95@gmail.com>
2022-12-31 15:42:33 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -7,10 +7,23 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
 
-        Scanner sc = new Scanner(System.in);
-        BigInteger a = new BigInteger(sc.next());
-        BigInteger b = new BigInteger(sc.next());
-        System.out.println(a.add(b).toString());
+        String[] alphabet = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String a : alphabet) {
+            map.put(a, -1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            String ss = s.substring(i, i + 1);
+            //System.out.println(i+" "+ss);
+            map.put(ss, i);
+        }
+        for (String key : map.keySet()) {
+            System.out.print(map.get(key)+" ");
+        }
     }
 }
