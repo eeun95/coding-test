c13cb86630a347e6f81924910763c898853e7ac9
eeun <eeun95@gmail.com>
2023-01-01 18:24:57 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,18 +6,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        s = s.toLowerCase();        // 소문자로 다 바꿔 ~!
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int R = Integer.parseInt(st.nextToken());
-            String s = st.nextToken();
-
-            for (int j = 0; j < s.length(); j++) {
-                String ss = s.substring(j, j + 1);
-                System.out.print(ss.repeat(R));
-            }
-            System.out.println();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 97; i <= 122; i++) {
+            String ch = String.valueOf((char) i);
+            map.put(ch, 0);
         }
+
+        for (int i = 0; i < s.length(); i++) {
+            String ss = s.substring(i, i + 1);
+            map.put(ss, map.get(ss) + 1);
+        }
+
+        List<String> list = new ArrayList<>(map.keySet());
+
+        Collections.sort(list, (value1, value2) -> (map.get(value2).compareTo(map.get(value1))));
+
+        int count = 0;
+        int max = 0;
+        String result = "";
+        for (String k : list) {
+            if(count > 1) break;
+            if(count==0) {
+                max = map.get(k);
+                result = k;
+            }
+            if(count==1 && max == map.get(k)) {
+                result = "?";
+            }
+            count++;
+        }
+        System.out.println(result.toUpperCase());
     }
 }
