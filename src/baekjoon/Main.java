e17783c95d493cb1ff9ac0ca2a450b0476b9c933
eeun <eeun95@gmail.com>
2023-01-02 17:13:23 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,38 +1,43 @@
 package baekjoon;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        int[] arr = new int[26]; // 영문자의 개수는 26개임
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int c = System.in.read();
-
-        while (c > 64) {	// 공백을 입력받는 순간 종료됨
-
-            if (c < 91) {
-                arr[c - 65]++;
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            String cmd = br.readLine();
+            if (cmd.startsWith("push")) {
+                String[] ss = cmd.split(" ");
+                s.push(Integer.parseInt(ss[1]));
+            } else if (cmd.startsWith("pop")) {
+                if (s.size() < 1) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(s.pop());
+                }
+            } else if (cmd.startsWith("size")) {
+                System.out.println(s.size());
+            } else if (cmd.startsWith("empty")) {
+                if (s.isEmpty()) {
+                    System.out.println("1");
+                } else {
+                    System.out.println("0");
+                }
             } else {
-                arr[c - 97]++;
-            }
-            c = System.in.read();
-        }
-
-
-        int max = -1;
-        int ch = -2;	// ? 는 63 이다.
-
-        for (int i = 0; i < 26; i++) {
-
-            if (arr[i] > max) {
-                max = arr[i];
-                ch = i;
-            } else if (arr[i] == max) {
-                ch = -2;
+                if (s.size() < 1) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(s.peek());
+                }
             }
         }
-        System.out.print((char) (ch+65));
     }
-
 }
