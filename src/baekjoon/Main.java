e4793dd8e9eb84784a82c367c82c7560abbbc980
eeun <eeun95@gmail.com>
2023-01-06 14:46:18 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -9,29 +9,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        StringBuffer sb = new StringBuffer();
+        String s = String.valueOf(sb.append(st.nextToken() + " " + st.nextToken()).reverse());
+        System.out.println(Math.max(Integer.parseInt(s.split(" ")[0]), Integer.parseInt(s.split(" ")[1])));
 
-
-        int [][] a = new int[N][M];
-        int [][] b = new int[N][M];
-        for (int i = 0; i < N *2 ; i++) {
-            int idx = i;
-            String [] s = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                if(i>=N) {
-                    idx = i-N;
-                    b[idx][j] = Integer.parseInt(s[j]);
-                } else {
-                    a[idx][j] = Integer.parseInt(s[j]);
-                }
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(a[i][j]+b[i][j]+" ");
-            }
-            System.out.println();
-        }
     }
 }
