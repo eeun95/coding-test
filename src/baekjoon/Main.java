d4f7baa5acdbb600680019d5c5cc22e55acdf59e
eeun <eeun95@gmail.com>
2023-01-08 20:44:02 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -9,13 +9,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] a = new int[N];
+        ArrayList<Integer> a = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            a[i] = Integer.parseInt(br.readLine());
+            a.add(Integer.parseInt(br.readLine()));
         }
-        Arrays.sort(a);
+        // Arrays.sort 사용하면 시간 초과
+        Collections.sort(a);
+        StringBuilder sb = new StringBuilder();
         for (int i : a) {
-            System.out.println(i);
+            // 하나씩 출력하면 시간초과 나기때문에 빌더 사용
+            sb.append(a).append("\n");
         }
+        System.out.println(sb);
     }
 }
