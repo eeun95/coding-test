6ebff201322d72452d0d7d31530f5b8d92f1f45f
eeun <eeun95@gmail.com>
2023-01-08 20:39:29 +0900

    백준

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -8,16 +8,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-
-        Integer[] a = new Integer[s.length()];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = Integer.parseInt(s.substring(i, i + 1));
+        int N = Integer.parseInt(br.readLine());
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(br.readLine());
         }
-
-        Arrays.sort(a, Comparator.reverseOrder());
+        Arrays.sort(a);
         for (int i : a) {
-            System.out.print(i);
+            System.out.println(i);
         }
     }
 }
