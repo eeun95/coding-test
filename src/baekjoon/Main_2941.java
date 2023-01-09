28517977a46409d03ff4bec18b3a37140bcbd7a6
eeun <eeun95@gmail.com>
2023-01-09 22:57:24 +0900

    백준

---
+++ src/baekjoon/Main_2941.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main_2941 {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("c=");
+        list.add("c-");
+        list.add("dz=");
+        list.add("lj");
+        list.add("nj");
+        list.add("s=");
+        list.add("z=");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int count = 0;
+        for (String s1 : list) {
+            int i = s.length() - s.replace(s1, "").length();
+            count += (i/s1.length());
+            s = s.replace(s1, "");
+        }
+        System.out.println(count+s.length());
+    }
+}
