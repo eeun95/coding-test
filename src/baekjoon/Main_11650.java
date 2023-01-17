be2209afe0b9e9a40c613bad0524385702821fd2
eeun <eeun95@gmail.com>
2023-01-17 11:08:21 +0900

    백준

--- src/baekjoon/Main_11650.java
+++ src/baekjoon/Main_11650.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_11650 {
@@ -10,9 +11,23 @@ public class Main_11650 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
+        int[][] a = new int[N][2];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            a[i][0] = Integer.parseInt(st.nextToken());
+            a[i][1] = Integer.parseInt(st.nextToken());
+        }
 
+        Arrays.sort(a, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(a[i][0]+" "+a[i][1]);
         }
     }
 }
