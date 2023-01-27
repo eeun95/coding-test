b2a8b015560db10fe5d508e4219a771132b12bac
eeun <eeun95@gmail.com>
2023-01-27 22:23:37 +0900

    백준

--- src/baekjoon/Main_1181.java
+++ src/baekjoon/Main_1181.java
@@ -3,8 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main_1181 {
     public static void main(String[] args) throws IOException {
@@ -16,6 +15,23 @@ public class Main_1181 {
             String s = br.readLine();
             map.put(s, s.length());
         }
+
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                //System.out.println(o1.getKey() + " " + o1.getValue() + " / " + o2.getKey() + " " + o2.getValue());
+                if(o1.getValue() == o2.getValue()) {
+                    return o1.getKey().compareTo(o2.getKey());
+                } else {
+                    return o1.getValue() - o2.getValue();
+                }
+            }
+        });
+        for (Map.Entry<String, Integer> entry : entryList) {
+            System.out.println(entry.getKey());
+        }
+
 
     }
 }
