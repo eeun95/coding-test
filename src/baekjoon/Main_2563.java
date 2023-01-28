cde022dc19f0d23803ce8026c91c918e0be452cc
eeun <eeun95@gmail.com>
2023-01-28 18:44:00 +0900

    백준

---
+++ src/baekjoon/Main_2563.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[][] paper = new boolean[100][100];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < (x + 10); j++) {
+                for (int k = y; k < (y + 10); k++) {
+                    paper[j][k] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if(paper[i][j]) sum++;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
