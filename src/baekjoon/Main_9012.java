e50f27c967891ff42e23fb2c332296a4f2201647
eeun <eeun95@gmail.com>
2023-02-06 13:44:58 +0900

    백준

--- src/baekjoon/Main_9012.java
+++ src/baekjoon/Main_9012.java
@@ -3,22 +3,27 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class Main_9012 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            Stack<String> stack = new Stack<>();
+            boolean b = true;
             String s = br.readLine();
-            for (int j = 0; j < s.length(); j++) {
-                String ss = s.substring(j, j + 1);
-                stack.push(ss);
+            while(true) {
+                String newString = s.replace("()", "");
+                s = newString;
+                if (!newString.contains("()")) {
+                    if(!newString.equals("")) {
+                        //System.out.println("NO > " + newString);
+                        b = false;
+                    }
+                    break;
+                }
             }
-
+            System.out.println((b) ? "YES" : "NO");
         }
     }
 }
