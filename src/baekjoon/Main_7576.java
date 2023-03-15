2b3799c8e46d92b8cacdcd1ce632eb4fb4b81ce8
eeun95 <eeun95@gmail.com>
2023-03-15 22:19:17 +0900

    백준

--- src/baekjoon/Main_7576.java
+++ src/baekjoon/Main_7576.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 public class Main_7576 {
     static int M,N;
     static int[][] tomato;
+    static boolean[][] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,6 +18,8 @@ public class Main_7576 {
         N = Integer.parseInt(st.nextToken());       // 상자의 세로 칸
 
         tomato = new int[N][M];
+        visit = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int j=0;
@@ -25,6 +28,30 @@ public class Main_7576 {
                 j++;
             }
         }
-        System.out.println(Arrays.toString(tomato));
+        for (int i = 0; i<tomato.length; i++) {
+            for (int j = 0; j < tomato[i].length; j++) {
+                if(tomato[i][j] == 1) tomatoDelicious(i,j);
+            }
+        }
+    }
+
+    static void tomatoDelicious(int i, int j) {
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+
+        if(!visit[i][j]) {
+            System.out.println("HI?");
+            visit[i][j] = true;
+            for (int k = 0; k < 4; k++) {
+                int nx = i+dx[k];
+                int ny = j+dy[k];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if(!visit[nx][ny] && tomato[nx][ny] != -1) {
+                        tomatoDelicious(nx, ny);
+                    }
+                }
+            }
+        }
     }
 }
