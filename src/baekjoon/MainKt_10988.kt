02c95fe348587d989a93b1341181a754eeeebaee
eeun95 <eeun95@gmail.com>
2023-04-12 23:11:41 +0900

    백준

--- src/baekjoon/MainKt_10988.kt
+++ src/baekjoon/MainKt_10988.kt
@@ -3,19 +3,23 @@ package baekjoon
 fun main() {
     var s: String? = readLine()
 
-    var sArray: Array<String?> = arrayOfNulls(s!!.length)
-    for (i in 0 until s!!.length) {
-        sArray[i] = s.substring(i, i+1)
-    }
-
-    var result:Int = 1
-    var j:Int = s.length-1
-    for(i in 0 until s!!.length/2 step 1 step j.dec()) {
-        if(!sArray[i].equals(sArray[j])) {
-            result = 0
-            break
+    if(s!=null) {
+        var sArray: Array<String?> = arrayOfNulls(s.length)
+        for (i in 0 until s.length) {
+            sArray[i] = s.substring(i, i + 1)
         }
+
+        var result: Int = 1
+        var j: Int = s.length - 1
+        for (i in 0 until s.length / 2) {
+            //println(sArray[i]+" "+sArray[j])
+            if (!sArray[i].equals(sArray[j])) {
+                result = 0
+                break
+            }
+            j = j.dec()
+        }
+        print(result)
     }
-    print(result)
 
 }
