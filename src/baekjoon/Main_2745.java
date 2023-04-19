f8b9224445ce0b7f18b63aa0e307b64f4504636f
eeun95 <eeun95@gmail.com>
2023-04-20 00:36:14 +0900

    백준

--- src/baekjoon/Main_2745.java
+++ src/baekjoon/Main_2745.java
@@ -11,6 +11,21 @@ public class Main_2745 {
         String N = input[0];
         int B = Integer.parseInt(input[1]);
 
-        System.out.println(Math.pow(36,5));
+        // ex.ZZZZZ 36
+        // (35*36^4)+(35*36^3)+(35*36^2)+(35*36)+(35*1)
+
+        for (int i = 0; i < N.length(); i++) {
+            char c = N.charAt(i);
+            int ascii = 0;
+            if(c >= '0' && c <= '9') {
+                // 0 빼주는 이유는 '0'이 아스키코드 48이라서
+                // 숫자로 바꾼 아스키에 -48 해주는거랑 똑같
+                ascii = c-'0';
+            } else {
+                ascii = c-55;
+            }
+            System.out.println();
+        }
+
     }
 }
