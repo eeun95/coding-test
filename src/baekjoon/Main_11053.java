7c635984f7d86622b4a95ab468300704778d2250
eeun95 <eeun95@gmail.com>
2023-05-09 01:04:49 +0900

    백준

--- src/baekjoon/Main_11053.java
+++ src/baekjoon/Main_11053.java
@@ -5,29 +5,6 @@ import java.util.Scanner;
 
 public class Main_11053 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();   // 수열 A의 크기
-        int[] A = new int[N];
-        int[] dp = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < N; i++) {
-            dp[i] = 1;
-
-            for (int j = 0; j < i; j++) {
-                if(A[j] < A[i] && dp[i] < dp[j]+1) {
-                    dp[i] = dp[j]+1;
-                }
-            }
-        }
-
-        System.out.println(Arrays.stream(dp).max().getAsInt());
-        /*
-        * OptionalInt[4] -> getAsInt로 값을 꺼내줌
-        * intStream과 같은 기본 스트림은 Optional도 기본형을 값으로 하는 OptionalInt, OptionalLong, OptionalDouble을 반환한다
-        * */
     }
 }
--- src/baekjoon/Main_1932.java
+++ src/baekjoon/Main_1932.java
@@ -7,37 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main_1932 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-        int[][] triangle = new int[n][n];
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int j = 0;
-            while (st.hasMoreTokens()) {
-                triangle[i][j] = Integer.valueOf(st.nextToken());
-                j++;
-            }
-        }
 
-        int max = 0;
-
-        // 초기값 세팅
-        dp[0][0] = triangle[0][0];
-        for(int j=1; j<triangle.length; j++) {
-            for (int k = 0; k < triangle[j].length; k++) {
-                int now = triangle[j][k];
-                if(k==0)
-                    dp[j][k] = now+dp[j-1][k];
-                else
-                    dp[j][k] = Math.max(now+dp[j-1][k-1], now+dp[j-1][k]);
-                //System.out.println(dp[j][k]);
-            }
-        }
-        for (int m = 0; m < triangle.length; m++) {
-            max = Math.max(max, dp[n-1][m]);
-        }
-        System.out.println(max);
     }
 }
 
