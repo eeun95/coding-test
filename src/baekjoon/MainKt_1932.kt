c11eb73aaea5aa680e1f6c8d4b545eda01953c32
eeun95 <eeun95@gmail.com>
2023-05-13 00:28:42 +0900

    백준 다시 풀기 >0<

--- src/baekjoon/MainKt_1932.kt
+++ src/baekjoon/MainKt_1932.kt
@@ -1,9 +1,18 @@
 package baekjoon
 
+import java.util.StringTokenizer
+
 fun main() {
     val n = readLine()!!.toInt()
 
+    var dp : IntArray
     for (i in 0 until n) {
+        var st : StringTokenizer = StringTokenizer(readLine())
+        var cnt = 0
+        while (st.hasMoreTokens()) {
+            var num = st.nextToken().toInt()
 
+            cnt++
+        }
     }
 }
