32b99dd7589e01dadde1d851ea133eafe6b2cfc6
eeun95 <eeun95@gmail.com>
2023-05-13 00:24:46 +0900

    백준 다시 풀기 >0<

--- src/baekjoon/Main_1932.java
+++ src/baekjoon/Main_1932.java
@@ -8,20 +8,17 @@ import java.util.OptionalInt;
 import java.util.StringTokenizer;
 
 public class Main_1932 {
-    static int[][] array;
     static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         dp = new int[n][n];
-        array = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int cnt = 0;
             while(st.hasMoreTokens()) {
                 int num = Integer.parseInt(st.nextToken());
-                array[i][cnt] = num;
                 dp[i][cnt] = num;
                 cnt++;
             }
