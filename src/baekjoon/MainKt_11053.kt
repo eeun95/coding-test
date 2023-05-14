bedb988e98093f78d519e0c481fd946a4b600c96
eeun95 <eeun95@gmail.com>
2023-05-15 07:45:19 +0900

    백준 다시 풀기 >0<

---
+++ src/baekjoon/MainKt_11053.kt
@@ -0,0 +1,8 @@
+package baekjoon
+
+import java.lang.module.ModuleDescriptor.read
+
+fun main() {
+    var N = readLine()!!.toInt()
+
+}
--- src/baekjoon/Main_11053.java
+++ src/baekjoon/Main_11053.java
@@ -1,10 +1,13 @@
 package baekjoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main_11053 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
     }
 }
