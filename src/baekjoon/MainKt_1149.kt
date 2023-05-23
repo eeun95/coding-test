2ff7e7b59f43a0e671d7e29e8e78fe889b3758c7
eeun95 <eeun95@gmail.com>
2023-05-24 00:44:51 +0900

    백준 다시 풀기 >0<

--- src/baekjoon/MainKt_1149.kt
+++ src/baekjoon/MainKt_1149.kt
@@ -1,4 +1,24 @@
 package baekjoon
 
-class MainKt_1149 {
+import java.util.StringTokenizer
+
+private var dp = emptyArray<Array<Int>>()
+private var home = emptyArray<Array<Int>>()
+
+fun main() {
+    val N : Int = readLine()!!.toInt()
+    dp = Array(N) {
+        emptyArray()
+    }
+    home = Array(N) {
+        kotlin.emptyArray()
+    }
+
+    for (i in 0 until N) {
+        val st = StringTokenizer(readLine(), " ")
+        val cnt = 0
+        while(st.hasMoreTokens()) {
+            home[i][cnt] = st.nextToken().toInt()
+        }
+    }
 }
--- src/baekjoon/Main_1149.java
+++ src/baekjoon/Main_1149.java
@@ -3,11 +3,26 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_1149 {
     static int[][] dp;
-    static int[][] cost;
+    static int[][] home;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        home = new int[N][N];
+        dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int cnt = 0;
+            while(st.hasMoreTokens()) {
+                home[i][cnt] = Integer.parseInt(st.nextToken());
+                cnt++;
+            }
+        }
+
     }
 }
