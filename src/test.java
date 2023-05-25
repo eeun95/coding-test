cfd7fe84f95fabbc722e02ca46e542dd595a50ee
eeun95 <eeun95@gmail.com>
2023-05-26 00:29:08 +0900

    ㅌㅔ스트

--- src/test.java
+++ src/test.java
@@ -1,51 +1,32 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println(numberToString("240044000"));   //2만 4천
-        System.out.println(numberToString("240000"));   //24만
-        System.out.println(numberToString("2400000"));   //240만
-        System.out.println(numberToString("24000000"));   //2400만
-        System.out.println(numberToString("200040000"));   //2억 4천
-        //System.out.println(numberToString("2400000000"));   //24억
-        //System.out.println(numberToString("24000000000"));   //240억
-        //System.out.println(numberToString("240000000000"));   //2400백억
-        System.out.println(numberToString("684000"));   //68만 4천
+        int[] dots = new int[]{1, 5, 8};
+        int[] lines = new int[]{1, 3, 4, 6};
+        solution(dots, lines);
+
+        dots = new int[]{1, 3, 4, 6, 7, 10};
+        lines = new int[]{2, 2, 2, 2};
+        solution(dots, lines);
     }
-    public static String numberToString(String number) {
-        String result = "";
-        String[] unit = {"", "천", "만", "억", "조", "경"};
-        int len = number.length();
-        String[] s = new String[(len %4 ==0) ? len/4 : (len/4)+1];
+    public static int solution(int[] dots, int[] lines) {
+        int answer = 0;
 
-        if(len < 5) {
-            return number+"원";
-        }
-        int i=0;
-        int startIdx = 0;
-        int endIdx = (len%4==0) ? startIdx+4 : len%4;
-        while(len > 0) {
-            String n = number.substring(startIdx, endIdx);
-            s[i] = n;
-            startIdx = endIdx;
-            endIdx = startIdx+4;
-            len -= (endIdx - startIdx);
-            i++;
+        int[] betweenDots = new int[dots.length - 1];
+        for (int i = 0; i < dots.length - 1; i++) {
+            betweenDots[i] = dots[i + 1] - dots[i];
         }
 
-        i = 0;
-        for (String money : s) {
-            int m = Integer.parseInt(money);
-            if((s.length-i)!=1) {
-                result += (m + unit[(s.length - i)])+" ";
-            } else {
-                if(m > 0) {
-                    result += (m / 1000) + unit[(s.length - i)];
-                    result += (m%1000 > 0) ? (m%1000)+"원" : "원";
-                }
-            }
-            i++;
+        if (Arrays.stream(betweenDots).sum() > Arrays.stream(lines).sum()) {
+            return -1;
+        } else if(Arrays.stream(betweenDots).sum() == Arrays.stream(lines).sum()) {
+            return lines.length;
         }
 
-        return result;
+        return answer;
     }
-
 }
