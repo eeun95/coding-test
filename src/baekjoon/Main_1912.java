9bc82fbd230e690595e230ad0787ff2e8930e191
서은경 <eeun95@gmail.com>
2023-05-31 16:19:47 +0900

    백준 다시 풀기 ~~

--- src/baekjoon/Main_1912.java
+++ src/baekjoon/Main_1912.java
@@ -7,11 +7,13 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_1912 {
+    static int[] dp, array;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] array = new int[n];
+        array = new int[n];
+        dp = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int cnt = 0;
         while(st.hasMoreTokens()) {
@@ -21,6 +23,16 @@ public class Main_1912 {
 
         // 직전 수에 현재 수를 더한게 더큰지, 직전 수까지만 더하는게 더 큰지
         // ex. 10 -4 3 1 5 6 -35 12 21 -1
-        System.out.println(Arrays.toString(array));
+        dp[0] = array[0];
+        dp(9);
+        System.out.println(Arrays.toString(dp));
+
+    }
+    static int dp(int n) {
+        for (int i = 0; i < n; i++) {
+            dp[n] = Math.max(array[n]+dp(i), array[n]+array[n-1]);
+        }
+        System.out.println(dp[n]);
+        return dp[n];
     }
 }
