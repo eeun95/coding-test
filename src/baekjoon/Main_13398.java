44be6616eb8d7511b999f81c7ab207f1fe9d6a8a
서은경 <eeun95@gmail.com>
2023-06-01 17:52:13 +0900

    백준 다시 풀기 ~~

---
+++ src/baekjoon/Main_13398.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_13398 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
+        int[] dp = new int[N];
+
+        String[] s = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(s[i]);
+        }
+
+        // 연속된 수를 선택하여 가장 큰 합
+        // 수열에서 수를 하나 제거할 수 있다
+        // 첨부터 연속으로 더한 값에서 제일 작은 값 뺀것과 직전값+현재값 비교하기
+        // 10 -4 3 1 5 6 -35 12 21 -1
+        // 10+(-4)+3 vs 9
+
+        dp[0] = array[0];
+
+        for (int i = 1; i < N; i++) {
+            int min = Arrays.stream(dp).min().getAsInt();
+            dp[i] = Math.max(dp[i-1]+array[i], dp[i-1]+array[i]-min);
+        }
+        System.out.println(Arrays.toString(dp));
+    }
+}
