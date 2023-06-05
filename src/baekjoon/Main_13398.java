a7dd6768ea6a53b1b406bc24b465d4f3beaf9950
서은경 <eeun95@gmail.com>
2023-06-05 15:26:14 +0900

    백준 dp

--- src/baekjoon/Main_13398.java
+++ src/baekjoon/Main_13398.java
@@ -6,12 +6,14 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main_13398 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] array = new int[N];
         int[] dp = new int[N];
 
+        int[] dp2 = new int[N];
         String[] s = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
             array[i] = Integer.parseInt(s[i]);
@@ -26,19 +28,39 @@ public class Main_13398 {
         // 13+1 vs 10+(-4)+3+1(-4 제외) = 14
         // 14+5 vs 10+(-4)+3+1+5(-4 제외) = 19
 
+        // 6
+        // 3 -5 5 -4 5 4
+
+        // 8
+        // 1 -3 4 8 -4 -3 9 2 = 20
+
+        // 5
+        // 1 -3 4 8 -4 = 13
+
+        // 4
+        // 2 -1 2 -1000
 
         dp[0] = array[0];
+        dp2[0] = array[0];
 
         for (int i = 1; i < N; i++) {
             dp[i] = Math.max(dp[i-1]+array[i], array[i-1]+array[i]);
             //dp[i] = Math.max(Math.max(dp[i-1]+array[i], 0), Arrays.stream(tmp).sum()-min);
         }
-        int max = 0;
+        int max = Arrays.stream(dp).max().getAsInt();
+
         for (int i = 1; i < N; i++) {
-            int[] tmp = Arrays.copyOfRange(array, 0, i);
+            int[] tmp = Arrays.copyOfRange(array, 0, i+1);
             int min = Arrays.stream(tmp).min().getAsInt();
-            max = Math.max(dp[i], Arrays.stream(tmp).sum()-min);
+            dp2[i] = Math.max(Arrays.stream(tmp).sum()-min, dp2[i-1]+array[i]);
+            if(max < dp2[i]) {
+                max = dp2[i];
+            }
+            System.out.println(dp[i]+" "+(Arrays.stream(tmp).sum()-min)+" "+(dp2[i-1]+array[i]));
         }
+        System.out.println(Arrays.toString(dp));
+        System.out.println(Arrays.toString(dp2));
         System.out.println(max);
     }
+
 }
