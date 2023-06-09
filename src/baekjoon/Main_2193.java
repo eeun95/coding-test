c61790f4505a581dab18c5b568a54026f15631fd
서은경 <eeun95@gmail.com>
2023-06-09 18:29:33 +0900

    백준 dp

--- src/baekjoon/Main_2193.java
+++ src/baekjoon/Main_2193.java
@@ -2,7 +2,7 @@ package baekjoon;
 
 import java.util.Scanner;
 
-public class Main_2198 {
+public class Main_2193 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
