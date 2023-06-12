f122bbc1e4fff3f649e2c9c36953ecdb79149f14
eeun95 <eeun95@gmail.com>
2023-06-12 22:29:08 +0900

    백준 다시 풀기

--- src/baekjoon/MainKt_2193.kt
+++ src/baekjoon/MainKt_2193.kt
@@ -4,7 +4,8 @@ import java.util.*
 
 fun main() {
     val n = readLine()!!.toInt()
-    val dp = Array(n+1) { IntArray(2) }
+    val dp = Array(n+2) { LongArray(2) }
+    // n+1해주면 ArrayIndexOutOfBounds err
 
     // 0은 0으로 끝나는 이친수의 갯수
     // 1은 1로 끝나는 이친수의 갯수
@@ -13,12 +14,14 @@ fun main() {
     // 1
     // [10]
     // [100 101]
-    //
 
-    dp[1][0] = 0
-    dp[1][1] = 1
-    dp[2][0] = 1
-    dp[2][1] = 0
+    // 최댓값이 90인데 90넣으면 가질 수 있는 범위 초과!
+    // int 가 아니라 long으로 해줘야함
+
+    dp[1][0] = 0L
+    dp[1][1] = 1L
+    dp[2][0] = 1L
+    dp[2][1] = 0L
 
     for(i in 3 until n+1) {
         dp[i][0] = dp[i-2][0]+dp[i-1][0]
