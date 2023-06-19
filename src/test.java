0bfcc441099d743d379b5cb4e6d4764777757e9f
eeun95 <eeun95@gmail.com>
2023-06-20 00:30:35 +0900

    코테 연습.

--- src/test.java
+++ src/test.java
@@ -4,29 +4,38 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class test {
+    static long[] factorial;
     public static void main(String[] args) {
-        int[] dots = new int[]{1, 5, 8};
-        int[] lines = new int[]{1, 3, 4, 6};
-        solution(dots, lines);
+        double ceil = Math.round(((10/20)*10)/10);
+        System.out.println(Math.floor(31));
 
-        dots = new int[]{1, 3, 4, 6, 7, 10};
-        lines = new int[]{2, 2, 2, 2};
-        solution(dots, lines);
+        System.out.println(Math.round((double) 10 / (double) 20));
+        /*
+        factorial = new long[1000];
+        long n = fac(10);
+        System.out.println(n);
+        StringBuilder sb = new StringBuilder();
+        sb.append(362800);
+        StringBuilder reverse = sb.reverse();
+        boolean startFlag = true;
+        for (int i = 0; i < reverse.length(); i++) {
+            char c = (char) 0;
+            if(startFlag || reverse.charAt(i)=='0') {
+                startFlag = false;
+            } else {
+                break;
+            }
+        }
+
+         */
     }
-    public static int solution(int[] dots, int[] lines) {
-        int answer = 0;
-
-        int[] betweenDots = new int[dots.length - 1];
-        for (int i = 0; i < dots.length - 1; i++) {
-            betweenDots[i] = dots[i + 1] - dots[i];
+    static long fac(int n) {
+        if(n==1) {
+            factorial[1] = 1;
+            return factorial[1];
+        } else {
+            factorial[n] = n*fac(n-1);
+            return factorial[n];
         }
-
-        if (Arrays.stream(betweenDots).sum() > Arrays.stream(lines).sum()) {
-            return -1;
-        } else if(Arrays.stream(betweenDots).sum() == Arrays.stream(lines).sum()) {
-            return lines.length;
-        }
-
-        return answer;
     }
 }
