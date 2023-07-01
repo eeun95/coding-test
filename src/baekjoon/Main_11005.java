90e77c39da0e258d22464bd17e9236550577418b
eeun95 <eeun95@gmail.com>
2023-07-01 15:52:23 +0900

    코테 연습.

---
+++ src/baekjoon/Main_11005.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        // 10진법 수 N을 B진법으로 출력
+
+    }
+}
