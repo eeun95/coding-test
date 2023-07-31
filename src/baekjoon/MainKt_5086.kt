c92e985856497d2164a5127116fc6d3c3d0df722
서은경 <eeun95@gmail.com>
2023-07-31 14:15:51 +0900

    백준

---
+++ src/baekjoon/MainKt_5086.kt
@@ -0,0 +1,19 @@
+package baekjoon
+fun main() {
+    var br = readLine()!!
+    while (true) {
+        val s = br.split(" ")
+
+        if (s[0] == "0" && s[1] == "0") break
+        val first = s[0].toInt()
+        val second = s[1].toInt()
+        if (second % first == 0) {
+            println("factor")
+        } else if (first % second == 0) {
+            println("multiple")
+        } else {
+            println("neither")
+        }
+    }
+}
+
