84224421b1523d4985a4a0cae52e3255267bbdf8
서은경 <eeun95@gmail.com>
2023-09-01 18:19:09 +0900

    백준

---
+++ src/baekjoon/Main_1707.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1707 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            String s = br.readLine();
+            int V = Integer.parseInt(s.split(" ")[0]);
+            int E = Integer.parseInt(s.split(" ")[1]);
+
+            for(int j=0; j<E; j++) {
+                System.out.println(br.readLine());
+            }
+        }
+    }
+}
