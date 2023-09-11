b8eaefb0e582968c355c3663552a785e3295784e
eeun95 <eeun95@gmail.com>
2023-09-11 22:55:44 +0900

    stack

--- src/baekjoon/Main28278.java
+++ src/baekjoon/Main28278.java
@@ -10,11 +10,28 @@ public class Main28278 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < N; i++) {
             String command = br.readLine();
-
-
+            if(command.startsWith("1")) {
+                int x = Integer.parseInt(command.split(" ")[1]);
+                stack.push(x);
+            } else if(command.equals("2")) {
+                if(stack.isEmpty()) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(stack.pop()+"\n");
+                    //stack.stream().map(num -> sb.append(num+"\n"));
+                }
+            } else if(command.equals("3")) {
+                sb.append(stack.size()+"\n");
+            } else if(command.equals("4")) {
+                sb.append(stack.isEmpty() ? "1\n" : "0\n");
+            } else if(command.equals("5")) {
+                sb.append(stack.isEmpty() ? "-1\n" : stack.peek()+"\n");
+            }
         }
+        System.out.println(sb.toString());
     }
 }
